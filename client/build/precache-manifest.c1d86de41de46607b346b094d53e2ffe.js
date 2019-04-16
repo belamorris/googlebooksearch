569627563a7e8f3c0886212b77eb033f3a74ae46
Robert M <belamorris@gmail.com>
2019-04-15 18:27:57 -0600

    created initial files

---
+++ client/build/precache-manifest.c1d86de41de46607b346b094d53e2ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5277d2d7c6d028702cbf",
+    "url": "/static/js/main.ad5ea335.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5277d2d7c6d028702cbf",
+    "url": "/static/css/main.9e7c7c93.chunk.css"
+  },
+  {
+    "revision": "95099de287868a7120c20cd98db21907",
+    "url": "/index.html"
+  }
+];
